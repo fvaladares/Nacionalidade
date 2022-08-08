2d3f816d3fb47caa6ded7b431e94823e8ef88bbf
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-08-07 21:02:19 -0300

    Add copyright

--- app/src/main/java/br/pucminas/meunome/MainActivity.kt
+++ app/src/main/java/br/pucminas/meunome/MainActivity.kt
@@ -1,3 +1,26 @@
+/*
+ * Created/Adapted by fabricio on 07/08/2022 21:02
+ * Last modified 07/08/2022 21:02
+ *
+ * Copyright (C) 2022 Professor fabricio -- PUC Minas Virtual
+ *
+ * Code based on a example created by Google Developer Training
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package br.pucminas.meunome
 
 import android.os.Bundle
